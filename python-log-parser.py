4c4b4b642827624e49772d0d1429f4fb95081564
ivanov1van <iivanov@lineate.com>
2023-03-13 12:12:05 +0600

    issue with date convert and date math

--- python-log-parser.py
+++ python-log-parser.py
@@ -9,6 +9,7 @@
 # ---------------------------------------------------------------------------
 # Imports
 # ---------------------------------------------------------------------------
+import re
 # from ... import ...
 from sys import argv
 from datetime import datetime
@@ -16,6 +17,16 @@ from datetime import datetime
 # Передать аргументы скрипту путь / дата начала / дата завершения / сайт
 # Если забыли передать запросить ввод
 # Если не передали в обоих случаях вывести ошибку
+
+formats = [
+    "%d/%b/%Y:%H:%M" # 01/Jan/1999:00:00
+    "%d-%m-%Y",  # 05-03-2023
+    "%d %B %Y",  # 4 марта 2023
+    "%d %b %y",  # 4 мар. 23
+    "%d/%m/%Y",  # 05/03/2023
+]
+
+date_format = r'\b\d{1,4}[/-].{1,3}[/-]\d{1,4}[ :]\d{2}:\d{2}\b'
 
 if len(argv) > 1:
     path = argv[1]
@@ -30,16 +41,16 @@ else:
     print("Вы ввели путь до файла лога: ", path)
 
 if len(argv) > 1:
-    startdate = datetime.strptime(argv[2], '%d/%b/%Y')
+    startdate = datetime.strptime(argv[2], '%d/%b/%Y:%H:%M')
 else:
-    startdate = input("Введите дату начала дд/Ммм/гггг: ")
+    startdate = input("Введите дату начала дд/Ммм/гггг:00:00: ")
     try:
         # Преобразование введенной строки в объект datetime
-        date_check_format = datetime.strptime(startdate, '%d/%b/%Y')
+        date_check_format = datetime.strptime(startdate, '%d/%b/%Y:%H:%M')
         print("Дата: ", date_check_format)
     except ValueError:
         # Вывод сообщения об ошибке, если введенная строка не соответствует формату
-        print("Ошибка! Введенная дата не соответствует формату %d/%b/%Y, пример 01/Jan/1999")
+        print("Ошибка! Введенная дата не соответствует формату %d/%b/%Y:%H:%M, пример 01/Jan/1999:00:00")
 
 if not startdate:
     print("Ошибка: дата не была введена")
@@ -49,16 +60,16 @@ else:
     print("Вы ввели дату начала: ", startdate)
 
 if len(argv) > 1:
-    enddate = datetime.strptime(argv[3], '%d/%b/%Y')
+    enddate = datetime.strptime(argv[3], '%d/%b/%Y:%H:%M')
 else:
-    enddate = input("Введите дату окончания дд/Ммм/гггг: ")
+    enddate = input("Введите дату окончания дд/Ммм/гггг:чч:мм: ")
     try:
         # Преобразование введенной строки в объект datetime
-        date_check_format = datetime.strptime(enddate, '%d/%b/%Y')
+        date_check_format = datetime.strptime(enddate, '%d/%b/%Y:%H:%M')
         print("Дата: ", date_check_format)
     except ValueError:
         # Вывод сообщения об ошибке, если введенная строка не соответствует формату
-        print("Ошибка! Введенная дата не соответствует формату %d/%b/%Y, пример 01/Jan/1999")
+        print("Ошибка! Введенная дата не соответствует формату %d/%b/%Y:%H:%M, пример 01/Jan/1999:01:00")
 
 if not enddate:
     print("Ошибка: дата окончания не была введена")
@@ -78,3 +89,32 @@ if not site:
 else:
     # здесь можно выполнить действия с использованием аргумента
     print("Вы ввели сайт: ", site)
+
+"""Парсим файл лога за указанные даты"""
+
+# Открываем файл и читаем строки
+with open(path, 'r') as file:
+    lines = file.readlines()
+
+# Распознаем строки в соответствии с датами
+for line in lines:
+    # print(line.split()[3])
+    match = re.search(date_format, line.split()[3])
+    print(match)
+    convert = datetime.strptime(match,"%d/%b/%Y:%H:%M") # проблема
+    print(convert)
+    if match:
+        print(line.strip())
+        # Если дата находится в диапазоне между датами начала и окончания,
+        # выводим строку на экран
+        if startdate <= match.group() <= enddate: # проблема
+            print(line.strip())
+"""    try:
+        date_str = line.split(' ')[3] # Четвертое слово в строке содержит дату
+        # date = datetime.strptime(date_str, '%d/%b/%Y:%H:%M')
+        if startdate <= date <= enddate:
+            print(line.strip()) # Выводим строку без перевода строки
+    except ValueError:
+        # Если строка не начинается с даты, пропускаем ее
+        pass
+"""
